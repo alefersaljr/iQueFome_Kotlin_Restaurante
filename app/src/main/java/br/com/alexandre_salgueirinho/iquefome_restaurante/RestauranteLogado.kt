d9468ddeb8b8ac628490643374ae723b81a29f63
alefersaljr <a.salgueirinho@cinq.com.br>
2019-05-24 18:43:08 -0300

    24/05/2019
    - Criação do Trello do projeto
    - Criação da tela "Meus Dados"
    - Demais alterações

--- app/src/main/java/br/com/alexandre_salgueirinho/iquefome_restaurante/RestauranteLogado.kt
+++ app/src/main/java/br/com/alexandre_salgueirinho/iquefome_restaurante/RestauranteLogado.kt
@@ -3,14 +3,22 @@ package br.com.alexandre_salgueirinho.iquefome_restaurante
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_restaurante_logado.*
 
 class RestauranteLogado : AppCompatActivity() {
 
+    lateinit var mToolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurante_logado)
+
+        mToolbar = findViewById(R.id.logado_Toolbar)
+        setSupportActionBar(mToolbar)
 
         logado_Button_Add.setOnClickListener {
             goToAddFood()
@@ -30,5 +38,19 @@ class RestauranteLogado : AppCompatActivity() {
     private fun goToAddFood() {
         val intent = Intent (this, MainActivity::class.java)
         startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_menu_account, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_menu_icon_account -> {
+                startActivity(Intent(this, MeusDados::class.java))
+            }
+        }
+        return true
     }
 }
---
+++ app/src/main/java/br/com/alexandre_salgueirinho/iquefome_restaurante/RestauranteMeusDados.kt
@@ -0,0 +1,12 @@
+package br.com.alexandre_salgueirinho.iquefome_restaurante
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MeusDados : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meus_dados)
+    }
+}
