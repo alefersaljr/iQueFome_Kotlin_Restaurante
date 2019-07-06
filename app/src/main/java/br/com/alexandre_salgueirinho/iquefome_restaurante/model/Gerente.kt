d0bb7bf6d4f5fdc818f23f518f4d9721cbcec8f5
alefersaljr <a.salgueirinho@cinq.com.br>
2019-07-06 16:20:43 -0300

    06/07/2019 16:20

--- app/src/main/java/br/com/alexandre_salgueirinho/iquefome_restaurante/model/Gerente.kt
+++ app/src/main/java/br/com/alexandre_salgueirinho/iquefome_restaurante/model/Gerente.kt
@@ -1,15 +1,22 @@
 package br.com.alexandre_salgueirinho.iquefome_restaurante.model
 
-class Restaurante (
+class Gerente(
     val Id: String,
+    val gerente_Nome: String,
+    val gerente_Sobrenome: String,
+    val gerente_Celular: String,
+    val gerente_Email: String,
+    val gerente_Senha: String,
     val restaurante_Nome: String,
+    val restaurante_Celular: String,
     val restaurante_CEP: String,
     val restaurante_Cidade: String,
     val restaurante_Rua: String,
     val restaurante_Complemento: String,
     val restaurante_Email: String,
-    val restaurante_Senha: String,
     val tipoUser: String
 ) {
-    constructor () : this ("", "", "", "", "","", "", "", "")
+    constructor () : this("", "", "", "", "",
+        "", "", "", "", "",
+        "", "", "", "")
 }
